a8578e6bda6c3d0638f757e04f07299bd61a0903
lipi150 <112479235+lipi150@users.noreply.github.com>
2024-03-02 16:11:48 +0530

    first commit

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './Navbar.css'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      
+        <div>             
+           <Link to='/' className='login'><img src='' alt='Logo' /></Link>
+        </div>
+      <div className='div-2'>
+            <div className='login'><Link to='/loginpage' className='login'>Login</Link></div>
+      </div>
+
+    </div>
   )
 }
 
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div>
+        Homepage
+          
+    </div>
   )
 }
 
